b41d97a64b932cc54f763659b44159d8e29b23c6
Alexander Kurtev <allexander.k@gmail.com>
2012-12-05 20:45:09 +0200

    changes

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<title>Memory</title>
 	<link rel="stylesheet" type="text/css" href="css/memory.css">
 	<script type="text/javascript" src="js/lib/jquery-1.8.3.min.js"></script>
--- js/memory.js
+++ js/memory.js
@@ -1,3 +1,8 @@
+/* Create background */
+windowBackground = function() {
+	
+}
+
 /* display playfield */
 displayPlayField = function() {
 
@@ -5,14 +10,36 @@ displayPlayField = function() {
 	totalCellsNumber = $('#optform_hnumber').val() * $('#optform_vnumber').val();
 	newLineOn = $('#optform_hnumber').val();
 	
-	//console.log(totalCellsNumber);
-	lis = '';
+	li = '';
+
 	for(i=0; i<totalCellsNumber; i++){
-		lis += '<li></li>';
+
+		if(i % newLineOn == 0 )
+			li += '<li class="last"></li>';
+		else
+			li += '<li></li>';
 	}
-	cellsList.html(lis);
+	cellsList.html(li);
 }
 
+
+  $.fn.nodoubletapzoom = function() {
+      $(this).bind('touchstart', function preventZoom(e) {
+        var t2 = e.timeStamp
+          , t1 = $(this).data('lastTouch') || t2
+          , dt = t2 - t1
+          , fingers = e.originalEvent.touches.length;
+        $(this).data('lastTouch', t2);
+        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+        e.preventDefault(); // double tap - prevent the zoom
+        // also synthesize click events we just swallowed up
+        $(this).trigger('click').trigger('click');
+      });
+  };
+
+  $(document).nodoubletapzoom();
+  
 /* Start */
 $(function () {
 	displayPlayField();
